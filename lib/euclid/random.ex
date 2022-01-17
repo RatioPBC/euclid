c3be4a37f1e5e93178aad05a3d2a8943d45d622d
Erik Hanson <erik@eahanson.com>
2022-01-17 09:27:27 -0800

    `Euclid.Random.string` can now be base-32 encoded

--- lib/euclid/random.ex
+++ lib/euclid/random.ex
@@ -1,22 +1,38 @@
 defmodule Euclid.Random do
+  @type encoding() :: :base32 | :base64
   @moduledoc """
-  Helpers for generating random data in whatever format
+  Generate random data.
   """
 
   @doc """
-  Returns a base64 encoded string of given length
+  Returns a base64- or base32-encoded random string of 32 characters.
+  See `Euclid.Random.string/2`.
+  """
+  @spec string(encoding :: encoding()) :: binary()
+  def string(:base32), do: string(32, :base32)
+  def string(:base64), do: string(32, :base64)
+
+  @doc """
+  Returns a base64- or base32-encoded random string of given length
 
   ## Examples
       iex> Euclid.Random.string()
       "Sr/y4m/YiVSJcIgI5lG+76vMfaZ7KZ7c"
       iex> Euclid.Random.string(5)
       "9pJrK"
+      iex> Euclid.Random.string(5, :base32)
+      "AC53Z"
   """
-  @spec string(character_count :: pos_integer()) :: binary()
-  def string(character_count \\ 32) do
+  @spec string(character_count :: pos_integer(), encoding :: encoding()) :: binary()
+  def string(character_count \\ 32, encoding \\ :base64) when is_number(32) and encoding in [:base32, :base64] do
     character_count
     |> :crypto.strong_rand_bytes()
-    |> Base.encode64(padding: false)
+    |> encode(encoding)
     |> binary_part(0, character_count)
   end
+
+  # # #
+
+  defp encode(bytes, :base32), do: Base.encode32(bytes, padding: false)
+  defp encode(bytes, :base64), do: Base.encode64(bytes, padding: false)
 end
--- test/euclid/random_test.exs
+++ test/euclid/random_test.exs
@@ -2,6 +2,10 @@ defmodule Euclid.RandomTest do
   use Euclid.SimpleCase, async: true
 
   describe "string" do
+    test "defaults to 32 characters" do
+      assert String.length(Euclid.Random.string()) == 32
+    end
+
     test "returns a string with the given length" do
       assert String.length(Euclid.Random.string(5)) == 5
       assert String.length(Euclid.Random.string(59)) == 59
@@ -9,6 +13,22 @@ defmodule Euclid.RandomTest do
 
     test "returns strings that don't collide" do
       refute Euclid.Random.string(59) == Euclid.Random.string(59)
+    end
+
+    test "can be base64 encoded (the default)" do
+      for _ <- 1..1000 do
+        assert Euclid.Random.string() =~ ~r|^[A-Za-z0-9+/]{32}$|
+        assert Euclid.Random.string(:base64) =~ ~r|^[A-Za-z0-9+/]{32}$|
+        assert Euclid.Random.string(20) =~ ~r|^[A-Za-z0-9+/]{20}$|
+        assert Euclid.Random.string(10, :base64) =~ ~r|^[A-Za-z0-9+/]{10}$|
+      end
+    end
+
+    test "can be base32 encoded" do
+      for _ <- 1..1000 do
+        assert Euclid.Random.string(:base32) =~ ~r|^[A-Z2-7]{32}$|
+        assert Euclid.Random.string(20, :base32) =~ ~r|^[A-Z2-7]{20}$|
+      end
     end
   end
 end
